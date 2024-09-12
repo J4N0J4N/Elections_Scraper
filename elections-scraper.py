65c2ff98fa2f6f5761b632154b0b8dd9f853c830
J4N0J4N <jankovic.jan4@gmail.com>
2024-09-12 09:48:09 +0200

    Okrem hlasov vyscrapovane vsetko

--- elections-scraper.py
+++ elections-scraper.py
@@ -11,7 +11,7 @@ import csv
 import sys
 
 #nakoniec to napíšem do jednej funkcie
-url = "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=2&xnumnuts=2101"
+url = "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=12&xnumnuts=7103"
 response = requests.get(url)
 soup = bs(response.text, features="html.parser")
 
@@ -21,6 +21,8 @@ names = []
 parties = []
 hyperlinks = []
 registered = []
+envelopes = []
+valid = []
 
 # cyklus dynamicky vytvára listy pre cisla a nazvy(range 1 - 4 kvoli tomu, ze tabulka je rozdelena na 3 rovnake tabulky)
 for i in range(1, 4):
@@ -40,30 +42,18 @@ for municipality_url in hyperlinks:
     response = requests.get(municipality_url)
     municipality_soup = bs(response.text, 'html.parser')
 
+    # cyklus, ktorý priraduje do listu registered hodnoty z jednotlivych hyperlinkov
     registered += [registered_voters.text.strip().replace("\xa0", " ") for registered_voters in municipality_soup.find_all("td", {"headers": "sa2"})]
+    envelopes += [issued_envelopes.text.strip().replace("\xa0", " ") for issued_envelopes in municipality_soup.find_all("td", {"headers": "sa3"})]
+    valid += [valid_votes.text.strip().replace("\xa0", " ") for valid_votes in municipality_soup.find_all("td", {"headers": "sa6"})]
 
 # cyklus dynamicky vytvára list pre politické strany(range 1 - 3 kvoli tomu, ze tabulka je rozdelena na 2 rovnake tabulky)
 for i in range(1, 3):
     parties += [party.text.strip() for party in municipality_soup.find_all("td", headers=f"t{i}sa1 t{i}sb2")]
 
 
-#registered += [registered_voters.text.strip() for registered_voters in municipality_soup.find_all("td", {"headers": "sa2"})]
-#for registered_voters in municipality_soup.find_all("td", {"headers": "sa2", "data-rel": "L1"}):
-#    registered.append(registered_voters).text.strip()
-
-print(registered)
-
-"""registered = municipality_soup.find_all("td", {"headers": "sa2", "data-rel": "L1"})
-if registered:
-    print(registered)#.text.strip())
-else:
-    print("Prvok sa nenašiel.")
-"""
-
-
-
 # v premennej rows su ulozene jednotlive kombinacie kodov a nazvov obci do dvojic
-rows = zip(codes, names)
+rows = zip(codes, names, registered, envelopes, valid)
 
 # nemenna hlavicka csv
 header = ["Code", "Location", "Registered", "Envelopes", "Valid"]
@@ -72,5 +62,6 @@ header = ["Code", "Location", "Registered", "Envelopes", "Valid"]
 #zapis jednotlivych dvojic z premennej rows do prvych dvoch stlpcov
 with open("vysledky.csv", mode="w", encoding="UTF-8-sig") as new_csv:
     writer = csv.writer(new_csv, delimiter=";")
-    writer.writerow(header + parties)
+    writer.writerow(header+ parties)
     writer.writerows(rows)
+    #writer.writerow(registered)
